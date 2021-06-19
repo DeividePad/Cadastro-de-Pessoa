005fc3b08561a3c9b78cd42692b1fb2a72ba28d7
Deivide <Deivide@DESKTOP-8J0HBVR>
2021-06-18 22:45:40 -0300

    Adicionar arquivos de projeto.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace PropriedadesCheckBox
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPessoaFisica = new System.Windows.Forms.Panel();
+            this.txtCpfPessoaFisica = new System.Windows.Forms.TextBox();
+            this.txtNomePessoaFisica = new System.Windows.Forms.TextBox();
+            this.lblCpfPessoaFisica = new System.Windows.Forms.Label();
+            this.lblNomePessoaFisica = new System.Windows.Forms.Label();
+            this.pnlPessoaJuridica = new System.Windows.Forms.Panel();
+            this.txtCnpjPessoaJuridica = new System.Windows.Forms.TextBox();
+            this.txtNomePessoaJuridica = new System.Windows.Forms.TextBox();
+            this.lblCnpjPessoaJuridica = new System.Windows.Forms.Label();
+            this.lblNomePessoaJuridica = new System.Windows.Forms.Label();
+            this.RbPessoaFisica = new System.Windows.Forms.RadioButton();
+            this.RbPessoaJuridica = new System.Windows.Forms.RadioButton();
+            this.pnlPessoaFisica.SuspendLayout();
+            this.pnlPessoaJuridica.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlPessoaFisica
+            // 
+            this.pnlPessoaFisica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPessoaFisica.Controls.Add(this.txtCpfPessoaFisica);
+            this.pnlPessoaFisica.Controls.Add(this.txtNomePessoaFisica);
+            this.pnlPessoaFisica.Controls.Add(this.lblCpfPessoaFisica);
+            this.pnlPessoaFisica.Controls.Add(this.lblNomePessoaFisica);
+            this.pnlPessoaFisica.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlPessoaFisica.ForeColor = System.Drawing.Color.Black;
+            this.pnlPessoaFisica.Location = new System.Drawing.Point(28, 12);
+            this.pnlPessoaFisica.Name = "pnlPessoaFisica";
+            this.pnlPessoaFisica.Size = new System.Drawing.Size(210, 150);
+            this.pnlPessoaFisica.TabIndex = 0;
+            // 
+            // txtCpfPessoaFisica
+            // 
+            this.txtCpfPessoaFisica.Enabled = false;
+            this.txtCpfPessoaFisica.Location = new System.Drawing.Point(65, 87);
+            this.txtCpfPessoaFisica.Name = "txtCpfPessoaFisica";
+            this.txtCpfPessoaFisica.Size = new System.Drawing.Size(129, 22);
+            this.txtCpfPessoaFisica.TabIndex = 1;
+            // 
+            // txtNomePessoaFisica
+            // 
+            this.txtNomePessoaFisica.Enabled = false;
+            this.txtNomePessoaFisica.Location = new System.Drawing.Point(65, 24);
+            this.txtNomePessoaFisica.Name = "txtNomePessoaFisica";
+            this.txtNomePessoaFisica.Size = new System.Drawing.Size(129, 22);
+            this.txtNomePessoaFisica.TabIndex = 1;
+            // 
+            // lblCpfPessoaFisica
+            // 
+            this.lblCpfPessoaFisica.AutoSize = true;
+            this.lblCpfPessoaFisica.Location = new System.Drawing.Point(12, 90);
+            this.lblCpfPessoaFisica.Name = "lblCpfPessoaFisica";
+            this.lblCpfPessoaFisica.Size = new System.Drawing.Size(38, 16);
+            this.lblCpfPessoaFisica.TabIndex = 0;
+            this.lblCpfPessoaFisica.Text = "CPF:";
+            // 
+            // lblNomePessoaFisica
+            // 
+            this.lblNomePessoaFisica.AutoSize = true;
+            this.lblNomePessoaFisica.Location = new System.Drawing.Point(12, 27);
+            this.lblNomePessoaFisica.Name = "lblNomePessoaFisica";
+            this.lblNomePessoaFisica.Size = new System.Drawing.Size(49, 16);
+            this.lblNomePessoaFisica.TabIndex = 0;
+            this.lblNomePessoaFisica.Text = "Nome:";
+            // 
+            // pnlPessoaJuridica
+            // 
+            this.pnlPessoaJuridica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlPessoaJuridica.Controls.Add(this.txtCnpjPessoaJuridica);
+            this.pnlPessoaJuridica.Controls.Add(this.txtNomePessoaJuridica);
+            this.pnlPessoaJuridica.Controls.Add(this.lblCnpjPessoaJuridica);
+            this.pnlPessoaJuridica.Controls.Add(this.lblNomePessoaJuridica);
+            this.pnlPessoaJuridica.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlPessoaJuridica.Location = new System.Drawing.Point(276, 12);
+            this.pnlPessoaJuridica.Name = "pnlPessoaJuridica";
+            this.pnlPessoaJuridica.Size = new System.Drawing.Size(210, 150);
+            this.pnlPessoaJuridica.TabIndex = 0;
+            // 
+            // txtCnpjPessoaJuridica
+            // 
+            this.txtCnpjPessoaJuridica.Enabled = false;
+            this.txtCnpjPessoaJuridica.Location = new System.Drawing.Point(65, 87);
+            this.txtCnpjPessoaJuridica.Name = "txtCnpjPessoaJuridica";
+            this.txtCnpjPessoaJuridica.Size = new System.Drawing.Size(128, 22);
+            this.txtCnpjPessoaJuridica.TabIndex = 1;
+            // 
+            // txtNomePessoaJuridica
+            // 
+            this.txtNomePessoaJuridica.Enabled = false;
+            this.txtNomePessoaJuridica.Location = new System.Drawing.Point(65, 24);
+            this.txtNomePessoaJuridica.Name = "txtNomePessoaJuridica";
+            this.txtNomePessoaJuridica.Size = new System.Drawing.Size(128, 22);
+            this.txtNomePessoaJuridica.TabIndex = 1;
+            // 
+            // lblCnpjPessoaJuridica
+            // 
+            this.lblCnpjPessoaJuridica.AutoSize = true;
+            this.lblCnpjPessoaJuridica.Location = new System.Drawing.Point(12, 90);
+            this.lblCnpjPessoaJuridica.Name = "lblCnpjPessoaJuridica";
+            this.lblCnpjPessoaJuridica.Size = new System.Drawing.Size(46, 16);
+            this.lblCnpjPessoaJuridica.TabIndex = 0;
+            this.lblCnpjPessoaJuridica.Text = "CNPJ:";
+            // 
+            // lblNomePessoaJuridica
+            // 
+            this.lblNomePessoaJuridica.AutoSize = true;
+            this.lblNomePessoaJuridica.Location = new System.Drawing.Point(12, 27);
+            this.lblNomePessoaJuridica.Name = "lblNomePessoaJuridica";
+            this.lblNomePessoaJuridica.Size = new System.Drawing.Size(49, 16);
+            this.lblNomePessoaJuridica.TabIndex = 0;
+            this.lblNomePessoaJuridica.Text = "Nome:";
+            // 
+            // RbPessoaFisica
+            // 
+            this.RbPessoaFisica.AllowDrop = true;
+            this.RbPessoaFisica.AutoSize = true;
+            this.RbPessoaFisica.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RbPessoaFisica.Location = new System.Drawing.Point(44, 186);
+            this.RbPessoaFisica.Name = "RbPessoaFisica";
+            this.RbPessoaFisica.Size = new System.Drawing.Size(125, 22);
+            this.RbPessoaFisica.TabIndex = 1;
+            this.RbPessoaFisica.Text = "Pessoa Física";
+            this.RbPessoaFisica.UseVisualStyleBackColor = true;
+            this.RbPessoaFisica.CheckedChanged += new System.EventHandler(this.RbPessoaFisica_CheckedChanged);
+            // 
+            // RbPessoaJuridica
+            // 
+            this.RbPessoaJuridica.AllowDrop = true;
+            this.RbPessoaJuridica.AutoSize = true;
+            this.RbPessoaJuridica.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RbPessoaJuridica.Location = new System.Drawing.Point(292, 186);
+            this.RbPessoaJuridica.Name = "RbPessoaJuridica";
+            this.RbPessoaJuridica.Size = new System.Drawing.Size(141, 22);
+            this.RbPessoaJuridica.TabIndex = 1;
+            this.RbPessoaJuridica.Text = "Pessoa Jurídica";
+            this.RbPessoaJuridica.UseVisualStyleBackColor = true;
+            this.RbPessoaJuridica.CheckedChanged += new System.EventHandler(this.RbPessoaJuridica_CheckedChanged);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 252);
+            this.Controls.Add(this.RbPessoaJuridica);
+            this.Controls.Add(this.RbPessoaFisica);
+            this.Controls.Add(this.pnlPessoaJuridica);
+            this.Controls.Add(this.pnlPessoaFisica);
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Pessoa";
+            this.pnlPessoaFisica.ResumeLayout(false);
+            this.pnlPessoaFisica.PerformLayout();
+            this.pnlPessoaJuridica.ResumeLayout(false);
+            this.pnlPessoaJuridica.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPessoaFisica;
+        private System.Windows.Forms.TextBox txtCpfPessoaFisica;
+        private System.Windows.Forms.TextBox txtNomePessoaFisica;
+        private System.Windows.Forms.Label lblCpfPessoaFisica;
+        private System.Windows.Forms.Label lblNomePessoaFisica;
+        private System.Windows.Forms.Panel pnlPessoaJuridica;
+        private System.Windows.Forms.TextBox txtCnpjPessoaJuridica;
+        private System.Windows.Forms.TextBox txtNomePessoaJuridica;
+        private System.Windows.Forms.Label lblCnpjPessoaJuridica;
+        private System.Windows.Forms.Label lblNomePessoaJuridica;
+        private System.Windows.Forms.RadioButton RbPessoaFisica;
+        private System.Windows.Forms.RadioButton RbPessoaJuridica;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PropriedadesCheckBox
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void RbPessoaFisica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblNomePessoaFisica.ForeColor = Color.Blue;
+            lblCpfPessoaFisica.ForeColor = Color.Blue;
+            lblNomePessoaJuridica.ForeColor = Color.Black;
+            lblCnpjPessoaJuridica.ForeColor = Color.Black;
+            txtCpfPessoaFisica.Enabled = true;
+            txtNomePessoaFisica.Enabled = true;
+            txtNomePessoaJuridica.Enabled = false;
+            txtCnpjPessoaJuridica.Enabled = false;
+        }
+
+        private void RbPessoaJuridica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblNomePessoaJuridica.ForeColor = Color.Blue;
+            lblCnpjPessoaJuridica.ForeColor = Color.Blue;
+            lblNomePessoaFisica.ForeColor = Color.Black;
+            lblCpfPessoaFisica.ForeColor = Color.Black;
+            txtCpfPessoaFisica.Enabled = false;
+            txtNomePessoaFisica.Enabled = false;
+            txtNomePessoaJuridica.Enabled = true;
+            txtCnpjPessoaJuridica.Enabled = true;
+        }
+    }
+            
+}
